ce7e961071469ef4983764650d2c3904fe886821
redeboer <r.e.deboer@students.uu.nl>
2019-03-05 15:21:03 +0800

    New folder for testing the entire procedure of running jobs

---
+++ boss/workarea/job_tests/test_D0phi_KpiKK.sh
@@ -0,0 +1,8 @@
+# This script goes through the entire chain of running a package/job:
+#  1. Compile the package
+#  2. Source it through CMT
+#  3. Run the job using boss.exe and a debug job option script
+
+
+# * Script variables * #
+	PACKAGE="D0phi_KpiKK"
