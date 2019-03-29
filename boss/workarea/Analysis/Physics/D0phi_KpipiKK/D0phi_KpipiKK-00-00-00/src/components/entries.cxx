634395e8114dd44889deb4e2960de5ef5c96db0c
redeboer <r.e.deboer@students.uu.nl>
2019-03-29 08:25:55 +0800

    Copy: D0phi_KpiKK to D0phi_KpipiKK for added pi0 resonance

---
+++ boss/workarea/Analysis/Physics/D0phi_KpipiKK/D0phi_KpipiKK-00-00-00/src/components/entries.cxx
@@ -0,0 +1,8 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "D0phi_KpipiKK/D0phi_KpipiKK.h"
+
+DECLARE_ALGORITHM_FACTORY(D0phi_KpipiKK)
+
+DECLARE_FACTORY_ENTRIES(D0phi_KpipiKK) {
+  DECLARE_ALGORITHM(D0phi_KpipiKK);
+}
