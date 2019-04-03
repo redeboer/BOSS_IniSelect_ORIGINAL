7bde56a1438482a7e1769d315e1faed7ef6f1fc7
redeboer <r.e.deboer@students.uu.nl>
2019-04-03 08:09:49 +0800

    Moved derived classes of ConfigLoader to respective scripts

--- scripts/AnalyseInvMass.C
+++ scripts/AnalyseInvMass.C
@@ -1,12 +1,10 @@
-/// @addtogroup BOSS_Afterburner_scripts
-/// @{
 // * ======================================== * //
 // * ------- LIBRARIES AND NAMESPACES ------- * //
 // * ======================================== * //
 
-	#include "ConfigLoader_AnalyseInvMass.h"
 	#include "BOSSOutputLoader.h"
 	#include "CommonFunctions.h"
+	#include "ConfigLoader.h"
 	#include "TStyle.h"
 	#include <iostream>
 	#include <list>
@@ -21,9 +19,63 @@
 
 
 
+// * ================================= * //
+// * ------- CLASS DEFINITIONS ------- * //
+// * ================================= * //
+/// @addtogroup BOSS_Afterburner_objects
+/// @{
+
+
+	/// Derived class of `ConfigLoader` that contains parameters used for a simple plotting algorithm.
+	/// @author   Remco de Boer 雷穆克 (r.e.deboer@students.uu.nl or remco.de.boer@ihep.ac.cn)
+	/// @date     March 26th, 2019
+	class ConfigLoader_AnalyseInvMass : public ConfigLoader
+	{
+	public:
+		ConfigLoader_AnalyseInvMass(const std::string &path, const bool print=true) : ConfigLoader(path, print) { ConfigParBase::PrintAll();
+		LoadConfiguration(path);
+		}
+		/// @name I/O strings
+			///@{
+			ConfigParameter<std::string> Filename_excl{"Exclusive MC file"};
+			ConfigParameter<std::string> Filename_incl{"Inclusive MC file"};
+			ConfigParameter<std::string> Filename_data{"Measurements file"};
+			ConfigParameter<std::list<BranchPlotOptions> > ListOfbranches_excl{"Plot these inclusive MC branches"};
+			ConfigParameter<std::list<BranchPlotOptions> > ListOfbranches_incl{"Plot these exclusive MC branches"};
+			ConfigParameter<std::list<BranchPlotOptions> > ListOfbranches_data{"Plot these measurement branches"};
+			///@}
+
+
+		/// @name Do switches
+			///@{
+			ConfigParameter<bool> Do_PrintCutFlow{"Print cut flow", true}; ///< Whether or not to print an indication of the cut flow for the three files.
+			ConfigParameter<bool> Do_PureHists{"Draw and save histograms without fits", true};
+			ConfigParameter<bool> Do_Fit{"Draw and save fits", true};
+			///@}
+
+
+		/// @name Fit parameters
+			///@{
+			ConfigParameter<std::list<std::pair<ReconstructedParticle, BranchPlotOptions> > > ExclFits {"Signals to fit"};
+			///@}
+
+
+		/// @name Global plot style options
+			///@{
+			ConfigParameter<bool> Do_PlotStatsLegend{"Plot legend with statistics", false}; ///< Whether or not to draw the legend in the upper right corner with histogram statistics.
+			///@}
+
+
+	};
+
+
+
+/// @}
 // * ============================= * //
 // * ------- MAIN FUNCTION ------- * //
 // * ============================= * //
+/// @addtogroup BOSS_Afterburner_scripts
+/// @{
 
 
 	/// This script originates from `AnalyseInvMass` and is here for temporary developmental purposes.
@@ -101,6 +153,7 @@
 		else        D0phi_KpiKK(argv[1]);
 		return 0;
 	}
+
 
 
 /// @}
--- scripts/InvestigateContent.C
+++ scripts/InvestigateContent.C
@@ -1,21 +1,53 @@
-/// @addtogroup BOSS_Afterburner_scripts
-/// @{
 // * ======================================== * //
 // * ------- LIBRARIES AND NAMESPACES ------- * //
 // * ======================================== * //
 
 	#include "BOSSOutputLoader.h"
 	#include "CommonFunctions.h"
-	#include "ConfigLoader_InvestigateContent.h"
+	#include "ConfigLoader.h"
 	#include <iostream>
 
 	using namespace CommonFunctions;
 
 
 
+// * ================================= * //
+// * ------- CLASS DEFINITIONS ------- * //
+// * ================================= * //
+/// @addtogroup BOSS_Afterburner_objects
+/// @{
+
+
+	/// Derived class of `ConfigLoader` that contains parameters used for the `InvestigateContent` algorithm.
+	/// @author   Remco de Boer 雷穆克 (r.e.deboer@students.uu.nl or remco.de.boer@ihep.ac.cn)
+	/// @date     March 26th, 2019
+	class ConfigLoader_InvestigateContent : public ConfigLoader
+	{
+	public:
+		ConfigLoader_InvestigateContent(const std::string &path, const bool print=true) : ConfigLoader(path, print) { LoadConfiguration(path); }
+		/// @name I/O strings
+			///@{
+			ConfigParameter<std::string> fInputFilename{"Input file or directory", "/mnt/d/IHEP/root/D0phi_KpiKK/Jpsi2009+2012_dst"};
+			///@}
+
+
+		/// @name Do switches
+			///@{
+			ConfigParameter<bool> fPrintBranches{"Print branches", true}; ///< Whether or not to print all branch names.
+			ConfigParameter<bool> fPrintCutFlow{"Print cut flow", false}; ///< Whether or not to print the cut flow branch (if available).
+			///@}
+
+
+	};
+
+
+
+/// @}
 // * ============================= * //
 // * ------- MAIN FUNCTION ------- * //
 // * ============================= * //
+/// @addtogroup BOSS_Afterburner_scripts
+/// @{
 
 
 	/// Simple script that allows to quickly investigate the `TTree`s and branches of a ROOT file that has been produced with BOSS.
@@ -53,6 +85,7 @@
 		else        InvestigateContent(argv[1]);
 		return 0;
 	}
+
 
 
 /// @}
