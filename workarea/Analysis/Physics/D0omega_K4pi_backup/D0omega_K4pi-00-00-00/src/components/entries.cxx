20762cf0015a1438129cabb9763985d1ea651f0d
Remco de Boer <remco@ihep.ac.cn>
2019-05-30 15:40:16 +0800

    Removed D0omega_K4pi_backup

--- workarea/Analysis/Physics/D0omega_K4pi_backup/D0omega_K4pi-00-00-00/src/components/entries.cxx
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-#include "D0omega_K4pi/D0omega_K4pi.h"
-
-DECLARE_ALGORITHM_FACTORY(D0omega_K4pi)
-
-DECLARE_FACTORY_ENTRIES(D0omega_K4pi)
-{
-  DECLARE_ALGORITHM(D0omega_K4pi);
-}
--- workarea/Analysis/Physics/D0omega_K4pi_backup/D0omega_K4pi-00-00-00/src/components/load.cxx
+++
@@ -1,3 +0,0 @@
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES(D0omega_K4pi)
