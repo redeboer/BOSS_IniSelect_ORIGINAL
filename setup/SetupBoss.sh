e4506af7f1a4c0d655ae76bece69e497b5da32f2
Remco de Boer <remco@ihep.ac.cn>
2019-06-07 17:30:26 +0800

    Made setup script for BOSS more user friendly

--- setup/SetupBoss.sh
+++ setup/SetupBoss.sh
@@ -1,33 +1,53 @@
-# * Script parameters * #
-# ! Modify these
-BOSSVERSION="7.0.4"
-BOSSWORKAREA="/besfs/users/${USER}/BOSS_IniSelect/boss"
-BOSSWORKAREAbash="/besfs/users/\${USER}/BOSS_IniSelect/boss"
+echo "This script helps you 'install' BOSS:"
+
+read -p "  - Which version of BOSS do you want to load? (default is 7.0.4) " BOSSVERSION
+export BOSSVERSION=${BOSSVERSION:-7.0.4}
+
+read -e -p "  - What do you want to be your workarea? (default \"/besfs/users/${USER}\") " BOSSWORKAREA
+export BOSSWORKAREA=${BOSSWORKAREA:-/besfs/users/${USER}}
+
 CMTHOME="/afs/ihep.ac.cn/bes3/offline/Boss/cmthome/cmthome-${BOSSVERSION}"
 CMTHOMENAME="cmthome"
+WORKAREANAME="workarea"
 
 # * Check paramteres * #
 	if [ ! -d "${BOSSWORKAREA}" ]; then
 		echo "ERROR: Folder \"${BOSSWORKAREA}\" (BOSSWORKAREA) does not exist"
-		exit
+		exit 1
 	fi
 	if [ ! -d "${CMTHOME}" ]; then
-		echo "ERROR: Folder \"${CMTHOME}\" (CMTHOME) does not exist"
-		exit
+		echo "ERROR: Folder \"${CMTHOME}\" (CMTHOME) does not exist. Check chosen BOSS version..."
+		exit 1
 	fi
-	echo "Loading BOSS version ${BOSSVERSION}"
+	cmtPath="${BOSSWORKAREA}/${CMTHOMENAME}"
+	waPath="${BOSSWORKAREA}/${WORKAREANAME}"
+	echo
+	echo "Loading BOSS version ${BOSSVERSION} to path \"${cmtPath}\""
 
 # * Main script * #
 	# * Copy BOSS CMT files
-	cmtPath="${BOSSWORKAREA}/${CMTHOMENAME}"
-	mkdir -p "${cmtPath}" # create folder if necessary
-	rm -rf "${cmtPath}/"* # empty folder if necessary
-	cp -R "${CMTHOME}/"* "${cmtPath}" #! overwrite previous files!
+	if [[ -d "${cmtPath}" ]]; then
+		read -p "  CMT home folder \""${cmtPath}"\" already exists. Remove content ($(find ${cmtPath} -type f | wc -l) files)? (y/n)" -n 1 -r
+		echo
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			rm -rf "${cmtPath}"
+		fi
+		mkdir -p "${cmtPath}"
+	fi
+	if [[ -d "${waPath}" ]]; then
+		read -p "  Workarea home folder \""${waPath}"\" already exists. Remove content ($(find ${waPath} -type f | wc -l) files)? (y/n)" -n 1 -r
+		echo
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			rm -rf "${waPath}"
+		fi
+		mkdir -p "${waPath}"
+	fi
+	cp -R "${CMTHOME}/"* "${cmtPath}"
 	cd "${cmtPath}"
 
 	# * Replace lines in `requirements`
 	awk '{flag = 1}
-		{sub(/#macro WorkArea "\/ihepbatch\/bes\/maqm\/workarea"/,"macro WorkArea \"'${BOSSWORKAREAbash}'/workarea\"")}
+		{sub(/#macro WorkArea "\/ihepbatch\/bes\/maqm\/workarea"/,"macro WorkArea \"'${BOSSWORKAREA}'/workarea\"")}
 		{sub(/#path_remove CMTPATH  "\$\{WorkArea\}"/,"path_remove  CMTPATH \"${WorkArea}\"")}
 		{sub(/#path_prepend CMTPATH "\$\{WorkArea\}"/,"path_prepend CMTPATH \"${WorkArea}\"")}
 		{if(flag == 1) {print $0} else {next} }' \
@@ -35,15 +55,15 @@ CMTHOMENAME="cmthome"
 	mv  "${cmtPath}/requirements_new" "${cmtPath}/requirements"
 
 	# * Setup BOSS environment
-	echo -e "\n__________________"
+	echo
+	echo "__________________"
 	echo "source setupCMT.sh"
 	source setupCMT.sh  # starts connection to the CMT
-	echo -e "\n__________"
+	echo
+	echo "__________"
 	echo "cmt config"
 	cmt config          # initiates configuration
-	echo -e "\n_______________"
-	echo -e "source setup.sh"
+	echo
+	echo "_______________"
+	echo "source setup.sh"
 	source setup.sh     # sets path variables
-
-
-PrintSuccess \"${BASH_SOURCE[0]}\""
